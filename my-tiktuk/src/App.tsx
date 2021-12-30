0ad9dd17eaa484f3b1574e00ad384530d5b7f493
Margot <64271198+Margot-Jones@users.noreply.github.com>
2021-12-30 17:03:22 +0200

    Update App.tsx

--- my-tiktuk/src/App.tsx
+++ my-tiktuk/src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import ErrorCatcher from "./components/ErrorCatcher/ErrorCatcher";
 import Navbar from "./components/Navbar/Navbar";
 import Feed from "./pages/Feed";
